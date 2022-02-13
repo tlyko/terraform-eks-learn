50fa743825e280389a27721be5d23d2724fdda6b
Tomasz Łyko <tlyko94@gmail.com>
2022-02-13 14:13:34 +0100

    revert random eks name

--- vpc.tf
+++ vpc.tf
@@ -10,9 +10,13 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks"
+  cluster_name = "education-eks-${random_string.suffix.result}"
 }
 
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
