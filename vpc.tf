92c578563e2dc9517dfc2a8f9b966a90f01e197b
Tomasz Łyko <tlyko94@gmail.com>
2022-02-13 13:30:00 +0100

    rename cluster

--- vpc.tf
+++ vpc.tf
@@ -10,13 +10,9 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "education-eks"
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
